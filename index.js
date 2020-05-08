45558ec92ffe0d12e15d1eecc70f2d68101322f6
pachesnyi <vanya.pachesnyi@gmail.com>
2020-05-09 00:09:06 +0300

    ADD: fighters objects, methods, handlers

--- index.js
+++ index.js
@@ -1,24 +1,104 @@
-const API_URL = 'https://api.github.com/repos/sahanr/street-fighter/contents/fighters.json';
+const API_URL = 'https://api.github.com/';
 const rootElement = document.getElementById('root');
 const loadingElement = document.getElementById('loading-overlay');
+const fightersDetailsMap = new Map();
 
-fetch(API_URL)
-    .then( response => {
-        if(!response.ok) {
-            Promise.reject(new Error('Failed to load data'));
-        }
-        return response.json();
-    } )
-    .then(file => {
-        const fighters = JSON.parse(atob(file.content));
-        console.log(fighters);
-        const names = fighters.map(it => it.name).join('\n')
-        rootElement.innerText = names;
-    })
-    .catch(error=>{
+async function startApp() {
+    try {
+        loadingElement.style.visibility = 'visible';
+
+        const endpoint = 'repos/sahanr/street-fighter/contents/fighters.json';
+        const fighters = await callApi(endpoint, 'GET');
+        const fightersElement = createFighters(fighters);
+        rootElement.appendChild(fightersElement);
+    } catch (error) {
         console.warn(error);
         rootElement.innerText = 'Failed to load data';
-    })
-    .finally(()=>{
-        loadingElement.remove();
-    })
+    } finally {
+        loadingElement.style.visibility = 'hidden';
+    }
+}
+
+function callApi(endpoind, method) {
+    const url = API_URL + endpoind
+    const options = {
+        method
+    };
+
+    return fetch(url, options)
+        .then(response =>
+            response.ok
+                ? response.json()
+                : Promise.reject(Error('Failed to load'))
+        )
+        .then(file => JSON.parse(atob(file.content)))
+        .catch(error => { throw error });
+}
+
+function createElement({ tagName, className = '', attributes = {} }) {
+    const element = document.createElement(tagName);
+    element.classList.add(className);
+
+    Object
+        .keys(attributes)
+        .forEach(key => element.setAttribute(key, attributes[key]));
+
+    return element;
+}
+
+function createName(name) {
+    const nameElement = createElement({ tagName: 'span', className: 'name' });
+    nameElement.innerText = name;
+
+    return nameElement;
+}
+
+function createImage(source) {
+    const attributes = { src: source };
+    const imgElement = createElement({
+        tagName: 'img',
+        className: 'fighter-image',
+        attributes
+    });
+
+    return imgElement;
+}
+
+function createFighter(fighter) {
+    const { name, source } = fighter;
+    const nameElement = createName(name);
+    const imageElement = createImage(source);
+    const element = createElement({ tagName: 'div', className: 'fighter' });
+
+    element.addEventListener('click', (event) => handleFighterClick(event, fighter))
+    element.append(imageElement, nameElement);
+
+    return element;
+}
+
+function createFighters(fighters) {
+    const fighterElements = fighters.map(fighter => createFighter(fighter));
+    const element = createElement({ tagName: 'div', className: 'fighters' });
+
+    element.append(...fighterElements);
+
+    return element;
+}
+
+function handleFighterClick(event, fighter) {
+    const { _id } = fighter;
+  
+    if(!fightersDetailsMap.has(_id)) {
+      // send request here
+      fightersDetailsMap.set(_id, fighter);
+    }
+  
+    console.log(fightersDetailsMap.get(_id));
+  }
+
+function getFightersNames(fighters) {
+    const names = fighters.map(it => it.name).join('\n');
+    return names;
+}
+
+startApp();
